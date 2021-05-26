70325e6fc6a93df2552e15c1a3291e875c7fe5cd
Ari Stehney <ari.stehney@icloud.com>
2021-05-26 16:31:56 -0400

    Update Binary release system

--- generate.sh
+++ generate.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+cp index.json dist/
+mkdir dist
+
 for dir in sdcard/*/; do mkdir -- "$dir/SOUNDS"; done
-mkdir -p dist/sdcard
 for dir in sdcard/*/; do cp -r sounds/* "$dir/SOUNDS/"; done
 
-cp index.json dist/
-cp CNAME dist/
-
-for d in sdcard/* ; do
-    zip -r dist/$d.zip $d/*
+cd sdcard
+for d in * ; do
+    zip -r ../dist/$d.zip $d/*
 done
+cd ..
 
 for dir in sdcard/*/; do rm -rf -- "$dir/SOUNDS"; done
