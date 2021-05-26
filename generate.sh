61751b4ec5459550c3d5f1aaacc5df461c106b2f
Ari Stehney <ari.stehney@icloud.com>
2021-05-26 13:12:29 -0400

    Add gitignore and initial automation

--- generate.sh
+++ generate.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-mkdir sdcard/*/SOUNDS dist
-cp -r sounds/* sdcard/*/SOUNDS/
-cp sdcard/index.json dist/
+for dir in sdcard/*/; do mkdir -- "$dir/SOUNDS"; done
 
-for d in sdcard/*/ ; do
-    zip -r dist/$d.zip sdcard/$d
+mkdir -p dist/sdcard
+cp -r sounds/* sdcard/*/SOUNDS/
+cp index.json dist/
+cp CNAME dist/
+
+for d in sdcard/* ; do
+    zip -r dist/$d.zip $d
 done
+
+for dir in sdcard/*/; do rm -rf -- "$dir/SOUNDS"; done
