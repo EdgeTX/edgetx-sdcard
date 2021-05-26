f0913bc38b423c17402b17c51089dea9d6ec56a5
Ari Stehney <ari.stehney@icloud.com>
2021-05-26 13:33:19 -0400

    Fix Recursive Zip function

--- generate.sh
+++ generate.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 for dir in sdcard/*/; do mkdir -- "$dir/SOUNDS"; done
-
 mkdir -p dist/sdcard
-cp -r sounds/* sdcard/*/SOUNDS/
+for dir in sdcard/*/; do cp -r sounds/* "$dir/SOUNDS/"; done
+
 cp index.json dist/
 cp CNAME dist/
 
 for d in sdcard/* ; do
-    zip -r dist/$d.zip $d
+    zip -r dist/$d.zip $d/*
 done
 
 for dir in sdcard/*/; do rm -rf -- "$dir/SOUNDS"; done
