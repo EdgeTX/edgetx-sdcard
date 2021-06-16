515618044d5a916b3966e526d7ac88f7f7d3075c
Eder Nucci <edernucci@gmail.com>
2021-06-16 09:52:47 -0300

    Adding SOUNDS to SDCARD (#9)
    * First version with en and pt languages
    
    * Adding more languages

--- generate.sh
+++ generate.sh
@@ -3,22 +3,30 @@
 # include voice generator script
 . $(dirname "$0")/voice-gen.sh
 
-mkdir dist
-cp index.json dist/
-cp -r sdcard sdcard-build
+# mkdir dist
+# cp index.json dist/
+# cp -r sdcard sdcard-build
 
-for f in voices/*
-do
-  generate_lang $f en-IE-EmilyNeural
-done
+# for f in voices/*
+# do
+generate_lang voices/en-US-taranis.csv en-IE-EmilyNeural en
+generate_lang voices/pt-PT-taranis.csv pt-BR-FranciscaNeural pt
+generate_lang voices/es-ES-taranis.csv es-ES-ElviraNeural es
+generate_lang voices/it-IT-taranis.csv it-IT-ElsaNeural it
+generate_lang voices/de-DE-taranis.csv de-DE-KatjaNeural de
+generate_lang voices/fr-FR-taranis.csv fr-FR-DeniseNeural fr
+generate_lang voices/ru-RU-taranis.csv ru-RU-SvetlanaNeural ru
+generate_lang voices/cs-CZ-taranis.csv cs-CZ-VlastaNeural cz
 
-for dir in sdcard-build/*/; do mkdir -- "$dir/SOUNDS"; done
-for dir in sdcard-build/*/; do cp -r global/* "$dir/"; done
+# done
 
-cd sdcard-build
-for d in * ; do
-    zip -r ../dist/$d.zip $d/*
-done
-cd ..
+# for dir in sdcard-build/*/; do mkdir -- "$dir/SOUNDS"; done
+# for dir in sdcard-build/*/; do cp -r global/* "$dir/"; done
 
-rm -r sdcard-build
+# cd sdcard-build
+# for d in * ; do
+#     zip -r ../dist/$d.zip $d/*
+# done
+# cd ..
+
+# rm -r sdcard-build
