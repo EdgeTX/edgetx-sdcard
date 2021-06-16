14005319992b7a12fd61d9f415fa5c206d1126d7
Ari Stehney <42641932+CoderElectronics@users.noreply.github.com>
2021-06-16 09:40:12 -0400

    Fix SD Build script changes

--- generate.sh
+++ generate.sh
@@ -3,12 +3,10 @@
 # include voice generator script
 . $(dirname "$0")/voice-gen.sh
 
-# mkdir dist
-# cp index.json dist/
-# cp -r sdcard sdcard-build
+mkdir dist
+cp index.json dist/
+cp -r sdcard sdcard-build
 
-# for f in voices/*
-# do
 generate_lang voices/en-US-taranis.csv en-IE-EmilyNeural en
 generate_lang voices/pt-PT-taranis.csv pt-BR-FranciscaNeural pt
 generate_lang voices/es-ES-taranis.csv es-ES-ElviraNeural es
@@ -18,15 +16,12 @@ generate_lang voices/fr-FR-taranis.csv fr-FR-DeniseNeural fr
 generate_lang voices/ru-RU-taranis.csv ru-RU-SvetlanaNeural ru
 generate_lang voices/cs-CZ-taranis.csv cs-CZ-VlastaNeural cz
 
-# done
+for dir in sdcard-build/*/; do cp -r global/* "$dir/"; done
 
-# for dir in sdcard-build/*/; do mkdir -- "$dir/SOUNDS"; done
-# for dir in sdcard-build/*/; do cp -r global/* "$dir/"; done
+cd sdcard-build
+for d in * ; do
+    zip -r ../dist/$d.zip $d/*
+done
+cd ..
 
-# cd sdcard-build
-# for d in * ; do
-#     zip -r ../dist/$d.zip $d/*
-# done
-# cd ..
-
-# rm -r sdcard-build
+rm -r sdcard-build
