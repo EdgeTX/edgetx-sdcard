e3204bad47cca1856b197ba96b0e64968afe7336
Eder Nucci <eder@greenpass.com.br>
2021-06-15 21:13:57 -0300

    Adding more languages

--- generate.sh
+++ generate.sh
@@ -10,7 +10,14 @@
 # for f in voices/*
 # do
 generate_lang voices/en-US-taranis.csv en-IE-EmilyNeural en
-generate_lang voices/pt-BR-taranis.csv pt-BR-FranciscaNeural pt
+generate_lang voices/pt-PT-taranis.csv pt-BR-FranciscaNeural pt
+generate_lang voices/es-ES-taranis.csv es-ES-ElviraNeural es
+generate_lang voices/it-IT-taranis.csv it-IT-ElsaNeural it
+generate_lang voices/de-DE-taranis.csv de-DE-KatjaNeural de
+generate_lang voices/fr-FR-taranis.csv fr-FR-DeniseNeural fr
+generate_lang voices/ru-RU-taranis.csv ru-RU-SvetlanaNeural ru
+generate_lang voices/cs-CZ-taranis.csv cs-CZ-VlastaNeural cz
+
 # done
 
 # for dir in sdcard-build/*/; do mkdir -- "$dir/SOUNDS"; done
