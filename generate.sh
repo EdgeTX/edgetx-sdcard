178ad490da5992b385e5060f41e9d9371d10f046
Peter <pfeerick@users.noreply.github.com>
2021-06-17 15:10:44 +1000

    Add Azure creds, disable non-en langues for testing

--- generate.sh
+++ generate.sh
@@ -3,18 +3,21 @@
 # include voice generator script
 . $(dirname "$0")/voice-gen.sh
 
+spx config @key --set $AZURE_KEY
+spx config @region --set $AZURE_REGION
+
 mkdir dist
 cp index.json dist/
 cp -r sdcard sdcard-build
 
 generate_lang voices/en-US-taranis.csv en-IE-EmilyNeural en
-generate_lang voices/pt-PT-taranis.csv pt-BR-FranciscaNeural pt
-generate_lang voices/es-ES-taranis.csv es-ES-ElviraNeural es
-generate_lang voices/it-IT-taranis.csv it-IT-ElsaNeural it
-generate_lang voices/de-DE-taranis.csv de-DE-KatjaNeural de
-generate_lang voices/fr-FR-taranis.csv fr-FR-DeniseNeural fr
-generate_lang voices/ru-RU-taranis.csv ru-RU-SvetlanaNeural ru
-generate_lang voices/cs-CZ-taranis.csv cs-CZ-VlastaNeural cz
+# generate_lang voices/pt-PT-taranis.csv pt-BR-FranciscaNeural pt
+# generate_lang voices/es-ES-taranis.csv es-ES-ElviraNeural es
+# generate_lang voices/it-IT-taranis.csv it-IT-ElsaNeural it
+# generate_lang voices/de-DE-taranis.csv de-DE-KatjaNeural de
+# generate_lang voices/fr-FR-taranis.csv fr-FR-DeniseNeural fr
+# generate_lang voices/ru-RU-taranis.csv ru-RU-SvetlanaNeural ru
+# generate_lang voices/cs-CZ-taranis.csv cs-CZ-VlastaNeural cz
 
 for dir in sdcard-build/*/; do cp -r global/* "$dir/"; done
 
