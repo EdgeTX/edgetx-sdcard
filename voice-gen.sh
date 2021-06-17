b61baabb69a5410a6654e656a369ed36f12f15fd
Peter <pfeerick@users.noreply.github.com>
2021-06-17 16:09:05 +1000

    DEBUG: Echo command and bug out if fail

--- voice-gen.sh
+++ voice-gen.sh
@@ -11,7 +11,9 @@ generate_lang () {
           echo "File $filename already exists. Skipping."
       else
           echo "File $filename does not exists. Creating."
-          spx synthesize --text \""$text"\" --voice $2 --audio output global/SOUNDS/$3/SYSTEM/$filename #&& sleep 10
+          set -xe
+          spx synthesize --text \""$text"\" --voice $2 --audio output global/SOUNDS/$3/SYSTEM/$filename && sleep 10
+          set +xe
       fi
   done < $1
 }
