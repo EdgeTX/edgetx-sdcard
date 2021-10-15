498d45179158ec4ddc6f0039bd99128250485d12
offer-shmuely <oshmuely@cognyte.com>
2021-10-16 00:47:51 +0300

    update timer2 widget to app-mode (full screen mode)

--- sdcard/horus/WIDGETS/Timer2/main.lua
+++ sdcard/horus/WIDGETS/Timer2/main.lua
@@ -1,7 +1,7 @@
 -- A Timer version that fill better the widget area
 -- Offer Shmuely
--- Date: 2018
--- ver: 0.3
+-- Date: 2021
+-- ver: 0.4
 
 local options = {
   { "TextColor", COLOR, YELLOW },
@@ -119,17 +119,37 @@ end
 --- Zone size: 390x172 1/1
 --- Zone size: 460x252 1/1 (no sliders/trim/topbar)
 local function refreshZoneXLarge(wgt)
-  local t1 = model.getTimer(wgt.options.Timer-1)
+  local x = wgt.zone.x
+  local w = wgt.zone.w
+  local y = wgt.zone.y
+  local h = wgt.zone.h
 
+  local t1 = model.getTimer(wgt.options.Timer-1)
+  --print(t1.value)
   local timerInfo = getTimerHeader(wgt, t1)
-  lcd.drawText(wgt.zone.x, wgt.zone.y, timerInfo, SMLSIZE + CUSTOM_COLOR)
+  lcd.drawText(x, y, timerInfo, SMLSIZE + CUSTOM_COLOR)
 
   local time_str = formatTime(wgt, t1)
-  lcd.drawText(wgt.zone.x, wgt.zone.y+15, time_str, XXLSIZE + CUSTOM_COLOR)
+  lcd.drawText(x, y+15, time_str, XXLSIZE + CUSTOM_COLOR)
 end
 
 
-function refresh(wgt)
+--- Zone size: 460x252 (full screen app mode)
+local function refreshFullScreen(wgt, event, touchState)
+  local x = 0
+  local w = 460
+  local y = 0
+  local h = 252
+  
+  local t1 = model.getTimer(wgt.options.Timer-1)
+  --print(t1.value)
+  local timerInfo = getTimerHeader(wgt, t1)
+  lcd.drawText(10, 10, timerInfo, XXLSIZE + CUSTOM_COLOR)
+  local time_str = formatTime(wgt, t1)
+  lcd.drawText(120, 100, time_str, XXLSIZE + CUSTOM_COLOR)
+end
+
+local function refresh(wgt, event, touchState)
 
   if (wgt==nil) then
     print("refresh(nil)")
@@ -148,7 +168,9 @@ function refresh(wgt)
 
   lcd.setColor(CUSTOM_COLOR, wgt.options.TextColor)
 
-  if     wgt.zone.w  > 380 and wgt.zone.h > 165 then refreshZoneXLarge(wgt)
+  if (event ~= nil) then
+    refreshFullScreen(wgt, event, touchState)
+  elseif wgt.zone.w  > 380 and wgt.zone.h > 165 then refreshZoneXLarge(wgt)
   elseif wgt.zone.w  > 180 and wgt.zone.h > 145 then refreshZoneLarge(wgt)
   elseif wgt.zone.w  > 170 and wgt.zone.h >  65 then refreshZoneMedium(wgt)
   elseif wgt.zone.w  > 150 and wgt.zone.h >  28 then refreshZoneSmall(wgt)
