47b3eec3e99dea11af334d97bb85e233e39f8793
offer-shmuely <oshmuely@cognyte.com>
2021-10-23 22:28:41 +0300

    update Timer2 for horus - dynamic positioning to adapt to new layouts

--- sdcard/nv14/WIDGETS/Timer2/main.lua
+++ sdcard/nv14/WIDGETS/Timer2/main.lua
@@ -1,22 +1,22 @@
 -- A Timer version that fill better the widget area
 -- Offer Shmuely
 -- Date: 2021
--- ver: 0.4
+-- ver: 0.5
 
 local options = {
   { "TextColor", COLOR, YELLOW },
-  { "Timer", VALUE, 1, 1, 3 },
+  { "Timer", VALUE, 1, 1, 3},
   { "use_days", BOOL, 0 }   -- if greater than 24 hours: 0=still show as hours, 1=use days
 
 }
 
 local function log(s)
   return;
-  --print("app: " .. s)
+  --print("timer2: " .. s)
 end
 
 local function create(zone, options)
-  local wgt = { zone = zone, options = options }
+  local wgt  = { zone=zone, options=options}
   wgt.options.use_days = wgt.options.use_days % 2 -- modulo due to bug that cause the value to be other than 0|1
   return wgt
 end
@@ -34,7 +34,7 @@ end
 ------------------------------------------------------------
 
 local function formatTime(wgt, t1)
-  local dd_raw = t1.value / 86400 -- 24*3600
+  local dd_raw = t1.value/86400 -- 24*3600
   local dd = math.floor(dd_raw)
   local hh_raw = (dd_raw - dd) * 24
   local hh = math.floor(hh_raw)
@@ -44,19 +44,19 @@ local function formatTime(wgt, t1)
   local ss = math.floor(ss_raw)
   if dd == 0 and hh == 0 then
     -- less then 1 hour, 59:59
-    time_str = string.format("%02d:%02d", mm, ss)
+    time_str = string.format("%02d:%02d",mm, ss)
   elseif dd == 0 then
     -- lass then 24 hours, 23:59:59
-    time_str = string.format("%02d:%02d:%02d", hh, mm, ss)
+    time_str = string.format("%02d:%02d:%02d",hh, mm, ss)
   else
     -- more than 24 hours
 
     if wgt.options.use_days == 0 then
       -- 25:59:59
-      time_str = string.format("%02d:%02d:%02d", dd * 24 + hh, mm, ss)
+      time_str = string.format("%02d:%02d:%02d",dd*24 + hh, mm, ss)
     else
       -- 5d 23:59:59
-      time_str = string.format("%dd %02d:%02d:%02d", dd, hh, mm, ss)
+      time_str = string.format("%dd %02d:%02d:%02d",dd, hh, mm, ss)
     end
 
   end
@@ -77,19 +77,19 @@ end
 local function getFontSize(wgt, txt)
   local w,h = lcd.sizeText(txt, XXLSIZE)
   log(string.format("XXLSIZE w: %d, h: %d, %s", w,h, time_str))
-  if w < wgt.zone.w and h < wgt.zone.h then
+  if w < wgt.zone.w and h <= wgt.zone.h then
     return XXLSIZE
   end
 
   w,h = lcd.sizeText(txt, DBLSIZE)
   log(string.format("DBLSIZE w: %d, h: %d, %s", w,h, time_str))
-  if w < wgt.zone.w and h < wgt.zone.h then
+  if w < wgt.zone.w and h <= wgt.zone.h then
     return DBLSIZE
   end
 
   w,h = lcd.sizeText(txt, MIDSIZE)
   log(string.format("MIDSIZE w: %d, h: %d, %s", w,h, time_str))
-  if w < wgt.zone.w and h < wgt.zone.h then
+  if w < wgt.zone.w and h <= wgt.zone.h then
     return MIDSIZE
   end
 
@@ -106,27 +106,46 @@ local function refresh(wgt, event, touchState)
 
   local t1 = model.getTimer(wgt.options.Timer-1)
 
+  -- calculate timer info
   local timerInfo = getTimerHeader(wgt, t1)
-  lcd.drawText(wgt.zone.x, wgt.zone.y, timerInfo, SMLSIZE + CUSTOM_COLOR)
+  timer_info_w, timer_info_h = lcd.sizeText(timerInfo, SMLSIZE)
+  --lcd.drawText(wgt.zone.x, wgt.zone.y, timerInfo, SMLSIZE + CUSTOM_COLOR)
+
+  -- calculate timer time
   local time_str = formatTime(wgt, t1)
   local font_size = getFontSize(wgt, time_str)
   local zone_w = wgt.zone.w
   local zone_h = wgt.zone.h
-  if (event ~= nil) then
-    -- full screen
+
+  if (event ~= nil) then -- full screen
     font_size = XXLSIZE
     zone_w = 320
     zone_h = 480
   end
 
-  log(string.format("x=%d, y=%d, w=%d, h=%d", wgt.zone.x, wgt.zone.y, zone_w, zone_h))
+  local ts_w,ts_h = lcd.sizeText(time_str, font_size)
+  local dx = (zone_w - ts_w) /2
+  local dy = timer_info_h -1
+  local need_timer_info = true
+  if (timer_info_h + ts_h > zone_h) and (zone_h < 50) then
+    log(string.format("--- not enough height, do not display timer info"))
+    need_timer_info = false
+    dy = 0
+  end
 
-  w,h = lcd.sizeText(time_str, font_size)
-  local dx = (zone_w - w) /2
-  local dy = (zone_h - h) /2
-  log(string.format("dx: %d, dy: %d, zone_w: %d, w: %d)", dx, dy, zone_w , w))
+  log(string.format("timer_info: timer_info_x:%d, timer_info_h: %d", timer_info_w, timer_info_h))
+  log(string.format("x=%d, y=%d, w=%d, h=%d", wgt.zone.x, wgt.zone.y, zone_w, zone_h))
+  log(string.format("dx: %d, dy: %d, zone_w: %d, zone_h: %d, ts_w: %d, ts_h: %d)", dx, dy, zone_w ,zone_h , ts_w, ts_h))
+
+
+  -- draw timer info
+  if (need_timer_info) then
+    lcd.drawText(wgt.zone.x, wgt.zone.y, timerInfo, SMLSIZE + CUSTOM_COLOR)
+  end
+
+  -- draw timer time
   lcd.drawText(wgt.zone.x + dx, wgt.zone.y + dy, time_str, font_size + CUSTOM_COLOR)
 
 end
 
-return { name = "Timer2", options = options, create = create, update = update, background = background, refresh = refresh }
+return { name="Timer2", options=options, create=create, update=update, background=background, refresh=refresh }
