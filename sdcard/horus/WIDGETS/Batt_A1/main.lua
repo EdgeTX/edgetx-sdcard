03b690e33769c19be68e65ea3fa36f95a332965e
offer-shmuely <oshmuely@cognyte.com>
2021-12-03 00:47:23 +0200

    horus widget Batt_A1: update usage, and user option param name

--- sdcard/horus/WIDGETS/Batt_A1/main.lua
+++ sdcard/horus/WIDGETS/Batt_A1/main.lua
@@ -16,20 +16,38 @@
 ---- #                                                                       #
 ---- #########################################################################
 
--- Widget to display the levels of lipo/non-lipo battery from single analog source (like the VFAS/A1/A2/mini-quad-flight-controller)
+-- This widget display a graphical representation of a Lipo (not other types) battery level, it will automatically detect the cell amount of the battery.
+-- it will take a lipo voltage that received as a single value (as opposed to multi cell values send while using FLVSS liPo Voltage Sensor)
+-- common sources are:
+--   * Transmitter Battery
+--   * FrSky VFAS
+--   * A1/A2 analog voltage
+--   * mini quad flight controller
+--   * radio-master 168
+--   * OMP m2 heli
+
+
+-- Widget to display the levels of Lipo battery from single analog source
 -- Offer Shmuely
 -- Date: 2021
--- ver: 0.1
+-- ver: 0.2
 
 local _options = {
   { "Sensor", SOURCE, 0 }, -- default to 'A1'
   { "Color", COLOR, YELLOW },
-  { "Is_Lipo", BOOL, 1 } -- 0=show the voltage as is, 1=assume lipo, auto detect cell count, display as single cell
+  { "Show_Total_Voltage", BOOL, 0 } -- 0=Show as average Lipo cell level, 1=show the total voltage (voltage as is)
 }
 
 -- Data gathered from commercial lipo sensors
 local myArrayPercentList = { { 3, 0 }, { 3.093, 1 }, { 3.196, 2 }, { 3.301, 3 }, { 3.401, 4 }, { 3.477, 5 }, { 3.544, 6 }, { 3.601, 7 }, { 3.637, 8 }, { 3.664, 9 }, { 3.679, 10 }, { 3.683, 11 }, { 3.689, 12 }, { 3.692, 13 }, { 3.705, 14 }, { 3.71, 15 }, { 3.713, 16 }, { 3.715, 17 }, { 3.72, 18 }, { 3.731, 19 }, { 3.735, 20 }, { 3.744, 21 }, { 3.753, 22 }, { 3.756, 23 }, { 3.758, 24 }, { 3.762, 25 }, { 3.767, 26 }, { 3.774, 27 }, { 3.78, 28 }, { 3.783, 29 }, { 3.786, 30 }, { 3.789, 31 }, { 3.794, 32 }, { 3.797, 33 }, { 3.8, 34 }, { 3.802, 35 }, { 3.805, 36 }, { 3.808, 37 }, { 3.811, 38 }, { 3.815, 39 }, { 3.818, 40 }, { 3.822, 41 }, { 3.825, 42 }, { 3.829, 43 }, { 3.833, 44 }, { 3.836, 45 }, { 3.84, 46 }, { 3.843, 47 }, { 3.847, 48 }, { 3.85, 49 }, { 3.854, 50 }, { 3.857, 51 }, { 3.86, 52 }, { 3.863, 53 }, { 3.866, 54 }, { 3.87, 55 }, { 3.874, 56 }, { 3.879, 57 }, { 3.888, 58 }, { 3.893, 59 }, { 3.897, 60 }, { 3.902, 61 }, { 3.906, 62 }, { 3.911, 63 }, { 3.918, 64 }, { 3.923, 65 }, { 3.928, 66 }, { 3.939, 67 }, { 3.943, 68 }, { 3.949, 69 }, { 3.955, 70 }, { 3.961, 71 }, { 3.968, 72 }, { 3.974, 73 }, { 3.981, 74 }, { 3.987, 75 }, { 3.994, 76 }, { 4.001, 77 }, { 4.007, 78 }, { 4.014, 79 }, { 4.021, 80 }, { 4.029, 81 }, { 4.036, 82 }, { 4.044, 83 }, { 4.052, 84 }, { 4.062, 85 }, { 4.074, 86 }, { 4.085, 87 }, { 4.095, 88 }, { 4.105, 89 }, { 4.111, 90 }, { 4.116, 91 }, { 4.12, 92 }, { 4.125, 93 }, { 4.129, 94 }, { 4.135, 95 }, { 4.145, 96 }, { 4.176, 97 }, { 4.179, 98 }, { 4.193, 99 }, { 4.2, 100 } }
 local defaultSensor = "RxBt" -- RxBt / A1 / A3/ VFAS /RxBt
+
+--------------------------------------------------------------
+local function log(s)
+  return;
+  --print("Batt_A1: " .. s)
+end
+--------------------------------------------------------------
 
 -- This function is run once at the creation of the widget
 local function create(zone, options)
@@ -59,7 +77,7 @@ local function create(zone, options)
     wgt.options.Sensor = defaultSensor
   end
 
-  wgt.options.Is_Lipo = wgt.options.Is_Lipo % 2 -- modulo due to bug that cause the value to be other than 0|1
+  wgt.options.Show_Total_Voltage = wgt.options.Show_Total_Voltage % 2 -- modulo due to bug that cause the value to be other than 0|1
 
   return wgt
 end
@@ -77,7 +95,7 @@ local function update(wgt, options)
     wgt.options.Sensor = defaultSensor
   end
 
-  wgt.options.Is_Lipo = wgt.options.Is_Lipo % 2 -- modulo due to bug that cause the value to be other than 0|1
+  wgt.options.Show_Total_Voltage = wgt.options.Show_Total_Voltage % 2 -- modulo due to bug that cause the value to be other than 0|1
 
 end
 
@@ -233,10 +251,10 @@ local function calculateBatteryData(wgt)
   -- print("wgt.vPercent: ".. wgt.vPercent)
 
   -- mainValue
-  if wgt.options.Is_Lipo == 1 then
+  if wgt.options.Show_Total_Voltage == 0 then
     wgt.mainValue = wgt.vCellLive
     wgt.secondaryValue = wgt.vTotalLive
-  elseif wgt.options.Is_Lipo == 0 then
+  elseif wgt.options.Show_Total_Voltage == 1 then
     wgt.mainValue = wgt.vTotalLive
     wgt.secondaryValue = wgt.vCellLive
   else
@@ -407,7 +425,7 @@ local function refreshZoneMedium(wgt)
   end
   lcd.drawText(wgt.zone.x + myBatt.w + 10, wgt.zone.y, string.format("%2.1fV", wgt.mainValue), DBLSIZE + CUSTOM_COLOR + wgt.no_telem_blink)
   lcd.drawText(wgt.zone.x + myBatt.w + 10, wgt.zone.y + 30, string.format("%2.0f%%", wgt.vPercent), MIDSIZE + CUSTOM_COLOR + wgt.no_telem_blink)
-  if wgt.options.Is_Lipo == 1 then
+  if wgt.options.Show_Total_Voltage == 0 then
     lcd.drawText(wgt.zone.x + wgt.zone.w, wgt.zone.y + wgt.zone.h -35, string.format("%2.1fV %dS", wgt.secondaryValue, wgt.cellCount), RIGHT + SMLSIZE + CUSTOM_COLOR + wgt.no_telem_blink)
   else
     --lcd.drawText(wgt.zone.x, wgt.zone.y + 40, string.format("%2.1fV", wgt.mainValue), DBLSIZE + CUSTOM_COLOR + wgt.no_telem_blink)
@@ -523,7 +541,7 @@ local function refresh(wgt, event, touchState)
   if (wgt.zone == nil) then
     return
   end
-  if (wgt.options.Is_Lipo == nil) then
+  if (wgt.options.Show_Total_Voltage == nil) then
     return
   end
 
