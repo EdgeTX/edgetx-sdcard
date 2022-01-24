229d402535b942ecc53263237899bc24cc418323
Peter Feerick <peter.feerick@gmail.com>
2022-01-24 21:55:01 +1000

    Tweak generation script - shellcheck

--- generate.sh
+++ generate.sh
@@ -4,14 +4,17 @@ mkdir dist
 cp index.json dist/
 cp -r sdcard sdcard-build
 
-for dir in sdcard-build/*/; do cp -r global/* "$dir/"; done
-
-cd sdcard-build
-for d in * ; do
-    cd $d
-    zip -r ../../dist/$d.zip *
-    cd ..
+for dir in sdcard-build/*/; do
+    cp -r global/* "$dir/"
 done
-cd ..
+
+cd sdcard-build || exit
+
+for d in * ; do
+    cd "$d" || exit
+    zip -r "../../dist/$d.zip" ./*
+    cd .. || exit
+done
+cd .. || exit
 
 rm -r sdcard-build
