9298486a5a29a2bd5277268e70cbe671da87df7a
jfrickmann <jesper.frickmann@hotmail.com>
2022-03-03 07:19:43 -0500

    Added simple templates and matching Lua scripts that load Lua wizard scripts

---
+++ sdcard/horus/TEMPLATES/WIZARD/Plane.lua
@@ -0,0 +1,23 @@
+---- #########################################################################
+---- #                                                                       #
+---- # Copyright (C) EdgeTX                                                  #
+-----#                                                                       #
+---- # License GPLv2: http://www.gnu.org/licenses/gpl-2.0.html               #
+---- #                                                                       #
+---- # This program is free software; you can redistribute it and/or modify  #
+---- # it under the terms of the GNU General Public License version 2 as     #
+---- # published by the Free Software Foundation.                            #
+---- #                                                                       #
+---- # This program is distributed in the hope that it will be useful        #
+---- # but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+---- # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+---- # GNU General Public License for more details.                          #
+---- #                                                                       #
+---- #########################################################################
+
+local function run(event)
+  chdir("/SCRIPTS/WIZARD")
+  return "plane.lua"
+end
+
+return { run = run }
