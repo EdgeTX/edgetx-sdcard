72e5fb17ceb4d0a0a3f4b0a967a2fa2714bee466
Peter Feerick <peter.feerick@gmail.com>
2022-07-12 15:54:47 +1000

    feat(ghost): Report when no telem and no sensors Also optimises getting of sensor ID and indicates when space for widget is too small.

--- sdcard/c480x272/WIDGETS/Ghost/main.lua
+++ sdcard/c480x272/WIDGETS/Ghost/main.lua
@@ -22,25 +22,51 @@ local offsetX
 local offsetY
 local sensors = {}
 
+local id_RFMD
+local id_FRat
+local id_RQly
+local id_TPWR
+
+local id_VBan
+local id_VFrq
+local id_VChn
+local id_VPwr
+
+local updateIDs = true
+
 local options = {
   { "VTX", BOOL, 0 },
 }
 
 local function getValues(wgt)
   if wgt.options.VTX == 0 then
-    sensors[1] = string.format("%s", getValue("RFMD"))
-    sensors[2] = string.format("%d Hz", getValue("FRat"))
-    sensors[3] = string.format("%d %%", getValue("RQly"))
-    if (getValue("TPWR") == 0) then
+    sensors[1] = string.format("%s", getValue(id_RFMD))
+    sensors[2] = string.format("%d Hz", getValue(id_FRat))
+    sensors[3] = string.format("%d %%", getValue(id_RQly))
+    if (id_TPWR == nil) then
       sensors[4] = "Range"
     else
-      sensors[4] = string.format("%d mW", getValue("TPWR"))
+      sensors[4] = string.format("%d mW", getValue(id_TPWR))
     end
   else
-    sensors[1] = string.format("%s", getValue("VBan"))
-    sensors[2] = string.format("%dMHz", getValue("VFrq"))
-    sensors[3] = string.format("CH: %d", getValue("VChn"))
-    sensors[4] = string.format("%d mW", getValue("VPwr"))
+    sensors[1] = string.format("%s", getValue(id_VBan))
+    sensors[2] = string.format("%dMHz", getValue(id_VFrq))
+    sensors[3] = string.format("CH: %d", getValue(id_VChn))
+    sensors[4] = string.format("%d mW", getValue(id_VPwr))
+  end
+end
+
+local function getIDs(wgt)
+  id_RFMD = getFieldInfo("RFMD").id
+  if wgt.options.VTX == 0 then
+    id_FRat = getFieldInfo("FRat").id
+    id_RQly = getFieldInfo("RQly").id
+    id_TPWR = getFieldInfo("TPWR").id
+  else
+    id_VBan = getFieldInfo("VBan").id
+    id_VFrq = getFieldInfo("VFrq").id
+    id_VChn = getFieldInfo("VChn").id
+    id_VPwr = getFieldInfo("VPwr").id
   end
 end
 
@@ -56,14 +82,16 @@ end
 
 local function update(wgt, options)
   wgt.options = options
+  updateIDs = true
 end
 
 local function background(wgt)
 end
 
 function refresh(wgt)
-  -- runs onty on large enough zone
+  -- runs only on large enough zone
   if wgt.zone.w < 180 or wgt.zone.h < 145 then
+    lcd.drawText(1, 1, "Widget space too small!", LEFT)
     return
   end
 
@@ -72,7 +100,20 @@ function refresh(wgt)
   end
 
   if getRSSI() ~= 0 then
-    if getValue("RFMD") == "Pure" then
+    if getValue("RFMD") == 0 then
+      lcd.drawText(wgt.zone.x + offsetX + 85, wgt.zone.y + offsetY + 35, "SCAN", CENTER + DBLSIZE)
+      lcd.drawText(wgt.zone.x + offsetX + 85, wgt.zone.y + offsetY + 70, "TELEM", CENTER + DBLSIZE)
+      lcd.drawText(wgt.zone.x + offsetX + 85, wgt.zone.y + offsetY + 105, "SENSORS", CENTER + DBLSIZE)
+      updateIDs = true
+      return
+    end
+
+    if updateIDs == true then
+      getIDs(wgt)
+      updateIDs = false
+    end
+
+    if getValue(id_RFMD) == "Pure" then
       if orangeLed ~= nil then
         lcd.drawBitmap(orangeLed, wgt.zone.x + offsetX + 143, wgt.zone.y + offsetY)
       end
@@ -93,6 +134,9 @@ function refresh(wgt)
 
     -- Transmit power
     lcd.drawText(wgt.zone.x + offsetX + 85, wgt.zone.y + offsetY + 105, sensors[4], CENTER + DBLSIZE)
+  else
+    lcd.drawText(wgt.zone.x + offsetX + 85, wgt.zone.y + offsetY + 35, "NO", CENTER + DBLSIZE)
+    lcd.drawText(wgt.zone.x + offsetX + 85, wgt.zone.y + offsetY + 70, "TELEM", CENTER + DBLSIZE)
   end
 
 end
