773cb5ac87f984e54db56be35413d0435ecb3f54
oshmuely <AbCd321654>
2023-01-03 00:31:04 +0200

    automatic options value detection between versions (backward compatibility)

--- sdcard/c480x272/WIDGETS/Flights/main.lua
+++ sdcard/c480x272/WIDGETS/Flights/main.lua
@@ -43,16 +43,23 @@ local enable_count_announcement_on_start = 1 -- 0=no voice, 1=play the count upo
 local enable_count_announcement_on_end = 1   -- 0=no voice, 1=play the count upon end of flight
 local use_telemetry = 1                      -- 0=do not use telemetry, 1=use telemetry in state machine
 
-local options = {
+-- backward compatibility
+local ver, radio, maj, minor, rev, osname = getVersion()
+local DEFAULT_ARM_SWITCH_ID = 1
+local DEFAULT_MOTOR_CHANNEL_ID = 1
+if maj == 2 and minor == 7 then
     -- for 2.7.x
-    { "switch", SOURCE, 117 }, -- 117== SF (arm/safety switch)
-    { "motor_channel", SOURCE, 204 }, -- 204==CH3
-
+    DEFAULT_ARM_SWITCH_ID = 117     -- safety switch=SF
+    DEFAULT_MOTOR_CHANNEL_ID = 204  -- motor_channel=CH3
+elseif maj == 2 and minor >= 8 then
     -- for 2.8.x
-    --{ "switch", SOURCE, 125 }, -- 125== SF (arm/safety switch)
-    --{ "motor_channel", SOURCE, 212 }, -- 212==CH3
+    DEFAULT_ARM_SWITCH_ID = 125     -- safety switch=SF
+    DEFAULT_MOTOR_CHANNEL_ID = 212  -- motor_channel=CH3
+end
 
-
+local options = {
+    { "switch", SOURCE, DEFAULT_ARM_SWITCH_ID }, -- 117== SF (arm/safety switch)
+    { "motor_channel", SOURCE, DEFAULT_MOTOR_CHANNEL_ID }, -- 204==CH3
     { "min_flight_duration", VALUE, default_flight_starting_duration, 2, 120 },
     --{ "enable_sounds"    , BOOL  , 1      },  -- enable sound on adding succ flight, and on end of flight
     { "text_color", COLOR, YELLOW },
