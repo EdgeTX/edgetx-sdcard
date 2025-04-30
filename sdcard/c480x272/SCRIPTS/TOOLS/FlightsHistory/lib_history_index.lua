5f9ab12785fe86e5064b2a22cb6d927b4590867a
shmuely <offer.shmuely@gmail.com>
2025-04-30 22:30:34 +0300

    feat(FlightsHistory): introduce new LVGL GUI

--- sdcard/c480x272/SCRIPTS/TOOLS/FlightsHistory/lib_history_index.lua
+++ sdcard/c480x272/SCRIPTS/TOOLS/FlightsHistory/lib_history_index.lua
@@ -28,18 +28,44 @@ end
 local function insertFlight(flight_date, model_name, flight_count, duration, model_id)
     M.m_log.info("updateFlight(%s)", flight_date)
 
-    local new_fight = {
-        key = string.format("key:%s", m_utils.trim(flight_date)),
-        flight_date = m_utils.trim(flight_date),
-        model_name = m_utils.trim(model_name),
-        flight_count = tonumber(m_utils.trim(flight_count)),
-        duration = tonumber(m_utils.trim(duration)),
-        model_id = m_utils.trim(model_id),
-        desc = string.format("%s - %s #%d (%d min)", flight_date, model_name, flight_count, duration/60)
-        --desc = string.format("#%d %s - %s (%d min)", flight_count, flight_date, model_name, duration/60)
-        --desc = string.format("%s - #%d %s (%d min)", flight_date, flight_count,model_name, duration/60)
-    }
-    M.m_tables.list_ordered_insert(M.log_files_index_info, new_fight, compare_by_dates, 1)
+    local key = string.format("key:%s", m_utils.trim(flight_date))
+    local desc = string.format("%s - %s #%d (%d min)", flight_date, model_name, flight_count, duration/60)
+    --desc = string.format("#%d %s - %s (%d min)", flight_count, flight_date, model_name, duration/60)
+    --desc = string.format("%s - #%d %s (%d min)", flight_date, flight_count,model_name, duration/60)
+
+    local f_info = M.getFlightInfoByKey(key)
+
+    if f_info ~= nil then
+        f_info.duration = tonumber(m_utils.trim(duration))
+        f_info.desc = desc
+    else
+        local new_fight = {
+            key = key,
+            flight_date = m_utils.trim(flight_date),
+            model_name = m_utils.trim(model_name),
+            flight_count = tonumber(m_utils.trim(flight_count)),
+            duration = tonumber(m_utils.trim(duration)),
+            model_id = m_utils.trim(model_id),
+            desc = desc
+        }
+        M.m_log.info("4 updateFlight(%s) - ==%d== %s", key, new_fight.duration, new_fight.desc)
+        M.m_tables.list_ordered_insert(M.log_files_index_info, new_fight, compare_by_dates, 1)
+    end
+end
+
+function M.getFlightInfoByKey(key)
+    --M.m_log.info("------------- getFlightInfoByKey start")
+    local tbl = M.log_files_index_info
+    for i = 1, #tbl, 1 do
+        local f_info = tbl[i]
+        -- M.m_log.info("getFlightInfoByKey-" .. f_info.key)
+
+        if f_info.key == key then
+            M.m_log.info("getFlightInfoByKey- found key: " .. f_info.key)
+            return f_info
+        end
+    end
+    --M.m_log.info("------------- getFlightInfoByKey end")
 end
 
 function M.indexPrint(prefix)
@@ -100,11 +126,27 @@ function M.historyFileRead()
             local flight_count = m_utils.trim(values[3])
             local duration = m_utils.trim(values[4])
             local model_id = m_utils.trim(values[5])
-            M.m_log.info("indexRead: line: flight_date: %s, model_name: %s, flight_count: %s, duration: %s, model_id: %s",
+
+            -- if not a number, set to -1
+            if not tonumber(duration) then
+                duration = -1
+                M.m_log.info("indexRead: duration is NOT a number")
+            end
+
+            M.m_log.info(
+                "indexRead: line: flight_date: %s, model_name: %s, flight_count: %s, duration: %s, model_id: %s",
                 flight_date, model_name, flight_count, duration, model_id)
 
             --m_log.info("files_on_disk exist: %s", file_name)
             insertFlight(flight_date, model_name, flight_count, duration, model_id)
+        else
+            --# /models/model4.yml/duration=100.0
+            --# /models/model4.yml/flight_count=15
+
+            M.m_log.info("indexRead: comment: [%s]", line)
+            --local model_id, duration = string.match(line, "# /models/(%s*)/duration=(%d*)")
+            local mid = string.match(line, "(%s*)")
+            M.m_log.info("indexRead: # model_id: (%s), duration: [%s]", mid, "555")
         end
     end
 
